24fa7afa4020b677d0ab3f866957a421d7948dfa
Lorenzo Uliano <blazeylost@outlook.com>
2021-11-15 23:25:22 -0300

    show-delete-posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUpdatePostRequest;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -15,7 +14,6 @@ class PostController extends Controller
         
         return view('admin.posts.index', compact('posts'));
     }
-    
     public function create()
     {
         return view('admin.posts.create');
@@ -26,5 +24,30 @@ class PostController extends Controller
         $post = Post::create($request->all());
 
         return redirect()->route('posts.index');
+    }
+    public function show($id)
+    {
+        // $post = Post::where('id', $id)->first();
+
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+
+    }
+    public function destroy($id)
+    {
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+
+        $post->delete();
+
+
+        return redirect()
+                ->route('posts.index')
+                ->with('message', 'Post deletado com sucesso');
+
     }
 }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,10 +7,9 @@
             @endforeach
         </ul>
     @endif
-
 <form action="{{ route('posts.store') }}" method="post">
     @csrf
     <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo" >{{ old('content') }}</textarea>
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ old('content') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
